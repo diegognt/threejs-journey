c35cd4f37f0d5525bbf9085946f83d3bc2804020
Diego Navarro <diego.nava07@gmail.com>
2022-12-08 16:05:13 -0500

    Lesson 05, Transform objects.
    This Pr includes:
      - Quick menu listing available pages.
      - Code for Lesson 5.
      - Style for navigation

---
+++ src/transform-objects/main.ts
@@ -0,0 +1,67 @@
+import '../style.css';
+
+import { Scene, BoxGeometry, MeshBasicMaterial, Mesh, PerspectiveCamera, WebGLRenderer, AxesHelper, Group } from 'three'
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')!
+
+// Scene
+const scene = new Scene()
+
+/**
+ * Objects
+ */
+const group = new Group()
+group.position.y = 0.5
+group.scale.y = 2
+group.rotation.y = 1
+scene.add(group)
+
+const cubeOne = new Mesh(
+  new BoxGeometry(1, 1, 1),
+  new MeshBasicMaterial({color: 0xff0000})
+)
+group.add(cubeOne)
+
+const cubeTwo = new Mesh(
+  new BoxGeometry(1, 1, 1),
+  new MeshBasicMaterial({color: 0x00ff00})
+)
+cubeTwo.position.set(2, 0, 0)
+group.add(cubeTwo)
+
+const cubeThree = new Mesh(
+  new BoxGeometry(1, 1, 1),
+  new MeshBasicMaterial({color: 0x0000ff})
+)
+cubeThree.position.set(-2, 0, 0)
+group.add(cubeThree)
+/**
+ * Sizes
+ */
+const sizes = {
+  width: 800,
+  height: 600
+}
+
+/**
+ * Axes helper
+ */
+const axesHelper = new AxesHelper()
+scene.add(axesHelper)
+
+/**
+ * Camera
+ */
+const camera = new PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+scene.add(camera)
+
+/**
+ * Renderer
+ */
+const renderer = new WebGLRenderer({
+  canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
