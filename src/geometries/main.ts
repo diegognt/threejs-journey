387b3028d5bb52fc99ace4c8c7333b0acff4625f
Diego Navarro <code@diegognt.dev>
2024-03-02 11:39:35 -0500

    Lint and format issues fixed

--- src/geometries/main.ts
+++ src/geometries/main.ts
@@ -1,43 +1,43 @@
-import '../style.css'
+import "../style.css";
 
 import {
-  Scene,
-  MeshBasicMaterial,
-  Mesh,
-  PerspectiveCamera,
-  WebGLRenderer,
   BufferAttribute,
   BufferGeometry,
-} from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+  Mesh,
+  MeshBasicMaterial,
+  PerspectiveCamera,
+  Scene,
+  WebGLRenderer,
+} from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector("canvas.webgl");
 
 if (!canvas) {
-  throw new Error('There is no canvas to be used.')
+  throw new Error("There is no canvas to be used.");
 }
 // Scene
-const scene = new Scene()
+const scene = new Scene();
 
 // Object
-const numberOfVertex = 3
-const amountOfGeometries = 50 * numberOfVertex * numberOfVertex
-const geometry = new BufferGeometry()
-const positionsArray = new Float32Array(amountOfGeometries)
+const numberOfVertex = 3;
+const amountOfGeometries = 50 * numberOfVertex * numberOfVertex;
+const geometry = new BufferGeometry();
+const positionsArray = new Float32Array(amountOfGeometries);
 
 for (let index = 0; index < amountOfGeometries; index++) {
-  positionsArray[index] = Math.random() - 0.5
+  positionsArray[index] = Math.random() - 0.5;
 }
 
-const positionsAttribute = new BufferAttribute(positionsArray, numberOfVertex)
-geometry.setAttribute('position', positionsAttribute)
+const positionsAttribute = new BufferAttribute(positionsArray, numberOfVertex);
+geometry.setAttribute("position", positionsAttribute);
 
 const cube = new Mesh(
   geometry,
-  new MeshBasicMaterial({ color: 0x0000ff, wireframe: true })
-)
-scene.add(cube)
+  new MeshBasicMaterial({ color: 0x0000ff, wireframe: true }),
+);
+scene.add(cube);
 
 /**
  * Sizes
@@ -45,56 +45,56 @@ scene.add(cube)
 const sizes = {
   width: window.innerWidth,
   height: window.innerHeight,
-}
+};
 
-window.addEventListener('resize', () => {
+window.addEventListener("resize", () => {
   // Update the size
-  sizes.width = window.innerWidth
-  sizes.height = window.innerHeight
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
 
   // Update camera
-  camera.aspect = sizes.width / sizes.height
-  camera.updateProjectionMatrix()
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
 
   // Update Renderer
-  renderer.setSize(sizes.width, sizes.height)
+  renderer.setSize(sizes.width, sizes.height);
   /**
    * To avoid performance issue caused by the device pixel ratio is recommended
    * to use a maximum of 2 as the device pixel ratio.
    */
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
 
-window.addEventListener('dblclick', () => {
+window.addEventListener("dblclick", () => {
   if (!document.fullscreenElement) {
-    canvas.requestFullscreen()
+    canvas.requestFullscreen();
   } else {
-    document.exitFullscreen()
+    document.exitFullscreen();
   }
-})
+});
 
 /**
  * Camera
  */
-const camera = new PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
-scene.add(camera)
+const camera = new PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
 
 // Controls
-const control = new OrbitControls(camera, canvas as HTMLElement)
-control.enableDamping = true
+const control = new OrbitControls(camera, canvas as HTMLElement);
+control.enableDamping = true;
 
 /**
  * Renderer
  */
-const renderer = new WebGLRenderer({ canvas: canvas })
-renderer.setSize(sizes.width, sizes.height)
+const renderer = new WebGLRenderer({ canvas: canvas });
+renderer.setSize(sizes.width, sizes.height);
 
 const tick = () => {
   // Render the scene
-  renderer.render(scene, camera)
+  renderer.render(scene, camera);
 
-  window.requestAnimationFrame(tick)
-}
+  window.requestAnimationFrame(tick);
+};
 
-tick()
+tick();
--- src/resizing/main.ts
+++ src/resizing/main.ts
@@ -1,32 +1,32 @@
-import '../style.css'
+import "../style.css";
 
 import {
-  Scene,
-  BoxGeometry,
-  MeshBasicMaterial,
-  Mesh,
-  PerspectiveCamera,
-  WebGLRenderer,
   AxesHelper,
-} from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+  BoxGeometry,
+  Mesh,
+  MeshBasicMaterial,
+  PerspectiveCamera,
+  Scene,
+  WebGLRenderer,
+} from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector("canvas.webgl");
 
 if (!canvas) {
-  throw new Error('There is no canvas to be used.')
+  throw new Error("There is no canvas to be used.");
 }
 
 // Scene
-const scene = new Scene()
+const scene = new Scene();
 
 // Object
 const cube = new Mesh(
   new BoxGeometry(1, 1, 1),
-  new MeshBasicMaterial({ color: 0xff0000 })
-)
-scene.add(cube)
+  new MeshBasicMaterial({ color: 0xff0000 }),
+);
+scene.add(cube);
 
 /**
  * Sizes
@@ -34,62 +34,62 @@ scene.add(cube)
 const sizes = {
   width: window.innerWidth,
   height: window.innerHeight,
-}
+};
 
-window.addEventListener('resize', () => {
+window.addEventListener("resize", () => {
   // Update the size
-  sizes.width = window.innerWidth
-  sizes.height = window.innerHeight
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
 
   // Update camera
-  camera.aspect = sizes.width / sizes.height
-  camera.updateProjectionMatrix()
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
 
   // Update Renderer
-  renderer.setSize(sizes.width, sizes.height)
+  renderer.setSize(sizes.width, sizes.height);
   /**
    * To avoid performance issue caused by the device pixel ratio is recommended
    * to use a maximum of 2 as the device pixel ratio.
    */
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
 
-window.addEventListener('dblclick', () => {
+window.addEventListener("dblclick", () => {
   if (!document.fullscreenElement) {
-    canvas.requestFullscreen()
+    canvas.requestFullscreen();
   } else {
-    document.exitFullscreen()
+    document.exitFullscreen();
   }
-})
+});
 
 /**
  * Axes helper
  */
-const axesHelper = new AxesHelper()
-scene.add(axesHelper)
+const axesHelper = new AxesHelper();
+scene.add(axesHelper);
 
 /**
  * Camera
  */
-const camera = new PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
-scene.add(camera)
+const camera = new PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
 
 // Controls
-const control = new OrbitControls(camera, canvas as HTMLElement)
-control.enableDamping = true
+const control = new OrbitControls(camera, canvas as HTMLElement);
+control.enableDamping = true;
 
 /**
  * Renderer
  */
-const renderer = new WebGLRenderer({ canvas: canvas })
-renderer.setSize(sizes.width, sizes.height)
+const renderer = new WebGLRenderer({ canvas: canvas });
+renderer.setSize(sizes.width, sizes.height);
 
 const tick = () => {
   // Render the scene
-  renderer.render(scene, camera)
+  renderer.render(scene, camera);
 
-  window.requestAnimationFrame(tick)
-}
+  window.requestAnimationFrame(tick);
+};
 
-tick()
+tick();
