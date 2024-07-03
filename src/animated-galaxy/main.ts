b969981270eeafd98ef9c6fb1836a154374653f0
Diego Navarro <code@diegognt.dev>
2024-07-03 17:14:24 -0500

    removes commented code

--- src/animated-galaxy/main.ts
+++ src/animated-galaxy/main.ts
@@ -13,38 +13,3 @@ galaxy.initialize();
 galaxy.setListeners();
 galaxy.draw();
 galaxy.tick();
-//
-// gui
-//   .add(parameters, "count")
-//   .min(100)
-//   .max(1000000)
-//   .step(100)
-//   .onFinishChange(displayGalaxy.bind(null, geometry, material, scene));
-// gui
-//   .add(parameters, "radius")
-//   .min(0.01)
-//   .max(20)
-//   .step(0.01)
-//   .onFinishChange(displayGalaxy.bind(null, geometry, material, scene));
-// gui
-//   .add(parameters, "branches")
-//   .min(2)
-//   .max(20)
-//   .step(1)
-//   .onFinishChange(displayGalaxy.bind(null, geometry, material, scene));
-// gui
-//   .add(parameters, "randomness")
-//   .min(0)
-//   .max(2)
-//   .step(0.001)
-//   .onFinishChange(displayGalaxy.bind(null, geometry, material, scene));
-// gui
-//   .add(parameters, "randomnessPower")
-//   .min(1)
-//   .max(10)
-//   .step(0.001)
-//   .onFinishChange(displayGalaxy.bind(null, geometry, material, scene));
-// // gui.addColor(parameters, "insideColor").onFinishChange(generateGeometry);
-// // gui.addColor(parameters, "outsideColor").onFinishChange(generateGeometry);
-//
-
